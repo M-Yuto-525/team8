3f994b2366f8e6f43f5cb384b0b5837ce4e54c5b
narunarunarumi <21129@hakodate.kosen-ac.jp>
2025-11-25 09:13:55 +0900

    FoodManとTenshuを追加

--- game8/foodman.java
+++ game8/foodman.java
@@ -1,15 +1,16 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class D here.
+ * Write a description of class A here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class D extends Actor
+public class foodman
+ extends Actor
 {
     /**
-     * Act - do whatever the D wants to do. This method is called whenever
+     * Act - do whatever the A wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- game8/tenshu1.java
+++ game8/tenshu1.java
@@ -1,15 +1,16 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class A here.
+ * Write a description of class C here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class A extends Actor
+public class tenshu1
+ extends Actor
 {
     /**
-     * Act - do whatever the A wants to do. This method is called whenever
+     * Act - do whatever the C wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
