463130341f01814e0b70f619656e39e540a121bb
mioto3010 <haoto0831@gmail.com>
2025-12-02 09:33:17 +0900

    水島君、鳴海くんのデータに店主２を書き換えたver

--- game8/Shoutengai.java
+++ game8/Shoutengai.java
@@ -18,14 +18,13 @@ public class Shoutengai extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 655, 1); 
-<<<<<<< HEAD
+
         
         tenshu1 enemy1 = new tenshu1(); 
         addObject(enemy1, 50, 50);
-=======
+        
         addObject(new foodman(), 0, 500);
         addObject(new tenshu1(), 100, 100);
         addObject(new tenshu2(), 500, 300);
->>>>>>> 3021f643270fda83b9f0de579a1baa2a5b423ffe
     }
 }
