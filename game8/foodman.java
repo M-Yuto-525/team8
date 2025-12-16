387a6855b555e99b76e65153b2574c9d52460984
Jrcomcom <kaito20050516@icloud.com>
2025-12-16 09:54:08 +0900

    add Wall collision

--- game8/foodman.java
+++ game8/foodman.java
@@ -3,19 +3,27 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class foodman extends Actor
 {
     private int speed = 3;
+    private String lastKeyPressed = "";
 
     // コンストラクタ（最初に1回だけ呼ばれる）
     public foodman() {
         GreenfootImage img = getImage();
-        img.scale(img.getWidth() / 4, img.getHeight() / 4);
+        img.scale(img.getWidth() / 2, img.getHeight() / 2);
         setImage(img);
     }
 
     public void act() 
     {
-        if (Greenfoot.isKeyDown("up"))    setLocation(getX(), getY() - speed);
-        if (Greenfoot.isKeyDown("down"))  setLocation(getX(), getY() + speed);
-        if (Greenfoot.isKeyDown("left"))  setLocation(getX() - speed, getY());
-        if (Greenfoot.isKeyDown("right")) setLocation(getX() + speed, getY());
+        if (Greenfoot.isKeyDown("up"))    setLocation(getX(), getY() - speed);lastKeyPressed="up";
+        if (Greenfoot.isKeyDown("down"))  setLocation(getX(), getY() + speed);lastKeyPressed="down";
+        if (Greenfoot.isKeyDown("left"))  setLocation(getX() - speed, getY());lastKeyPressed="left";
+        if (Greenfoot.isKeyDown("right")) setLocation(getX() + speed, getY());lastKeyPressed="right";
+        if(isTouching(Wall.class)){
+            if (Greenfoot.isKeyDown("up"))    setLocation(getX(), getY() + speed);
+            if (Greenfoot.isKeyDown("down"))  setLocation(getX(), getY() - speed);
+            if (Greenfoot.isKeyDown("left"))  setLocation(getX() + speed, getY());
+            if (Greenfoot.isKeyDown("right")) setLocation(getX() - speed, getY());
+        }
     }
+    
 }
